c428b205576d0ecbc829311b77ec147cab982c85
Andrew Krasichkov <dev@buglloc.cc>
2026-05-31 23:05:37 +0300

    std::move -> std::forward

--- include/defer.h
+++ include/defer.h
@@ -2,19 +2,25 @@
 
 #include "scope_guard.h"
 
+#include <utility>
 
-namespace
+
+namespace esbg::detail
 {
   struct DeferHelper {
     template<typename F>
     auto operator|(F&& fn) const {
-      return sg::make_scope_guard(std::move(fn));
+      return sg::make_scope_guard(std::forward<F>(fn));
     }
   };
-}
+} // namespace esbg::detail
 
 #define ESBG_DEFER__(x) ESBG_DEFER__ ## x
 #define ESBG_DEFER_(x) ESBG_DEFER__(x)
 
-#define ESBG_DEFER const auto ESBG_DEFER_(__COUNTER__) = DeferHelper() | [&]
+#define ESBG_DEFER [[maybe_unused]] const auto ESBG_DEFER_(__COUNTER__) \
+  = ::esbg::detail::DeferHelper{} | [&]
+
+#ifndef DEFER
 #define DEFER ESBG_DEFER
+#endif
